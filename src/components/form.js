e72a9e264755f090f5ef9f834bde0949cb8df22f
idil <idilcalislar@gmail.com>
2023-06-23 17:42:14 +0200

    components

---
+++ src/components/form.js
@@ -0,0 +1,28 @@
+export default function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new formData(event.target);
+    const data = {
+      name: formData.get("name"),
+      isForGoodWeather: formData.get("checkbox") === "on",
+    };
+    //const data = Object.fromEntries(formData);
+    event.target.reset();
+    event.target.elements.name.focus();
+    console.log(data);
+  }
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <h2>Add a new activity:</h2>
+        <label htmlFor="nameActivity"></label>
+        <input type="text" name="nameActivity" id="nameActivity" />
+        <label htmlFor="checkbox">Good weather activity?</label>
+        <input type="checkbox" name="checkbox" id="checkbox" />
+        <button type="submit" name="submit" id="submit">
+          SUBMIT
+        </button>
+      </form>
+    </>
+  );
+}
